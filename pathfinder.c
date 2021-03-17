8d1bdaffb5c8bc4136967e8d909e8976fb68a37e
Nam <p.n.nguyen@student.rug.nl>
2021-03-17 17:10:52 +0100

    initialize pseudo-distance

--- pathfinder.c
+++ pathfinder.c
@@ -5,5 +5,16 @@
 #include "pathfinder.h"
 
 void findPath(int graph[9][9], int v, int w){
+  int distance[12];
+  //S <- nodes(graph)
+
+  for(int i = 0; i < 12; i++){
+    if(i == v){
+      distance[i] = 0;
+    }
+    else{
+      distance[i] = -1;
+    }
+  }
 
 }
