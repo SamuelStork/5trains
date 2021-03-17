4acf81bc08f2afab4ca92478acd30f21ccf7a610
SamuelStork <s.dos.santos.stork@student.rug.nl>
2021-03-17 16:09:56 +0100

    Created ListNode and Graph structs

--- pathfinder.h
+++ pathfinder.h
@@ -1,6 +1,23 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
 
+
+
+typedef struct ListNode *ListPointer;
+
+typedef struct {
+	int node;
+	int weight;
+	ListPointer next;
+} ListNode;
+
+typedef struct{
+	ListNode n;
+	int data;
+	int visited;
+} Graph;
+
+Graph newGarph();
 void findPath();
 
 #endif
